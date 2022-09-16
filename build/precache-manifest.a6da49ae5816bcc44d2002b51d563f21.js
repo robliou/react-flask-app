f8bd3f1ced2836569eda1dd46a8e3a4922fab281
Rob L <81087205+flashrob01@users.noreply.github.com>
2022-09-16 13:39:54 +0800

    .

---
+++ build/precache-manifest.a6da49ae5816bcc44d2002b51d563f21.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "599e50edd345dc1b46e5080afa57d104",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04475953081399527278",
+    "url": "/static/css/2.6a2d8eaa.chunk.css"
+  },
+  {
+    "revision": "10d4b00b6a5e77b6c4c9",
+    "url": "/static/css/main.920624f9.chunk.css"
+  },
+  {
+    "revision": "04475953081399527278",
+    "url": "/static/js/2.f7b59fb2.chunk.js"
+  },
+  {
+    "revision": "a6a370b26ca9059d9bd5a72acd443901",
+    "url": "/static/js/2.f7b59fb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10d4b00b6a5e77b6c4c9",
+    "url": "/static/js/main.46f18ccc.chunk.js"
+  },
+  {
+    "revision": "4be130620b223c1c9c69",
+    "url": "/static/js/runtime-main.1cbfc7dc.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
